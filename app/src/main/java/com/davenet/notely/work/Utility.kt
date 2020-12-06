fdb05a8df221918e9e5b276baff48509f61dec01
shazomii <seyeoyeniran2u@gmail.com>
2020-12-06 15:20:16 +0100

    Move utility functions to a separate class

---
+++ app/src/main/java/com/davenet/notely/work/Utility.kt
@@ -0,0 +1,69 @@
+package com.davenet.notely.work
+
+import android.app.Activity
+import android.content.Context
+import androidx.work.Data
+import androidx.work.ExistingWorkPolicy
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.davenet.notely.R
+import com.davenet.notely.domain.NoteEntry
+import com.davenet.notely.util.Constants
+import com.davenet.notely.util.currentDate
+import kotlinx.android.synthetic.main.fragment_edit_note.*
+import petrov.kristiyan.colorpicker.ColorPicker
+import java.util.concurrent.TimeUnit
+
+class Utility {
+    fun pickColor(activity: Activity, note: NoteEntry) {
+        val colorPicker = ColorPicker(activity)
+        colorPicker.run {
+            setRoundColorButton(true)
+                .setTitle(activity.getString(R.string.note_color))
+                .show()
+            setOnChooseColorListener(object : ColorPicker.OnChooseColorListener {
+                override fun onChooseColor(position: Int, color: Int) {
+                    note.color = color
+                    activity.editNoteLayout.setBackgroundColor(color)
+                    activity.reminderCard.setCardBackgroundColor(color)
+                }
+
+                override fun onCancel() {
+                    return
+                }
+            })
+        }
+    }
+
+    fun createSchedule(context: Context, note: NoteEntry) {
+        val data = Data.Builder()
+            .putInt(Constants.NOTE_ID, note.id!!)
+            .putString(Constants.NOTE_TITLE, note.title)
+            .build()
+
+        val delay = note.reminder!! - currentDate().timeInMillis
+
+        note.started = true
+
+        scheduleReminder(delay, data, context)
+    }
+
+    private fun scheduleReminder(delay: Long, data: Data, context: Context) {
+        val reminderWork = OneTimeWorkRequest.Builder(NotifyWork::class.java)
+            .setInitialDelay(delay, TimeUnit.MILLISECONDS)
+            .setInputData(data)
+            .addTag("${context.packageName}.work.NotifyWork")
+            .build()
+
+        val workName = "Work ${data.getInt(Constants.NOTE_ID, 0)}"
+
+        val instanceWorkManager = WorkManager.getInstance(context)
+        instanceWorkManager.enqueueUniqueWork(workName, ExistingWorkPolicy.REPLACE, reminderWork)
+    }
+
+    fun cancelAlarm(context: Context, note: NoteEntry) {
+        val workName = "Work ${note.id}"
+        val instanceWorkManager = WorkManager.getInstance(context)
+        instanceWorkManager.cancelUniqueWork(workName)
+    }
+}
